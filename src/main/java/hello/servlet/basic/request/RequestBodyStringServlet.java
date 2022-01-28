f5bb722eb866f50ca96cc95f0431e96aabeadb39
leesh125 <chelsea12345@naver.com>
2022-01-28 23:33:34 +0900

    HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트(inputStream으로 전송한 메시지 바디의 text 읽기)

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Http 메시지 바디 부분을 바이트 코드로 바로 얻을 수 있음
+        ServletInputStream inputStream = request.getInputStream();
+        // byte 코드를 우리가 읽을 수 있는 문자(String)로 보려면 문자표(Charset)를 지정(UTF_8 Charset)
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+
+    }
+}
