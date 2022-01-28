338cf2c746902c7acb8f2e8e09c8b54fc74a461d
leesh125 <chelsea12345@naver.com>
2022-01-28 20:43:10 +0900

    Initial commit

---
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,16 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+// 프로젝트 패키지의 하위 패키지들 중에 서블릿을 찾아서 자동으로 등록한다
+@ServletComponentScan // 서블릿 자동 등록
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
