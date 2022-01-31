4f3242bbba21249c0b81c86a925224ad234565fb
leesh125 <chelsea12345@naver.com>
2022-01-31 18:33:32 +0900

    Model 추가 - v3 프론트 컨트롤러에서 modelView(model(데이터)과 viewName이 같이 있음) 생성해서 반환하고 -> 프론트는 modelView에 viewName으로 viewResolver를 호출하여 논리 view 이름을 물리로 변환 후 반환 -> 프론트는 view 를 렌더할 때 model 객체와 물리변환 viewname을 같이 넘김 -> model에 키 값을 model.setAttribute에 넘겨주고 jsp 실행

---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,67 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v2.ControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberFormControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberListControllerV2;
+import hello.servlet.web.frontcontroller.v2.controller.MemberSaveControllerV2;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// v1 하위에 어떤 url이 들어와도 일단 이 서블릿이 호출됨
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerV3Map = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        // 매개변수 1 url 요청이 오면 , MemberFormControllerV1 생성
+        controllerV3Map.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerV3Map.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerV3Map.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();// 들어온 URL을 얻을 수 있다.
+
+        ControllerV3 controller = controllerV3Map.get(requestURI); // 요청 온 URI에 의해 Controller 객체가 다르게 호출
+        if(controller == null){ // 읽어온 URI에 매치되는 Controller 객체가 없다면
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404 Not Found
+            return;
+        }
+
+        // paramMap 넘겨주기 ( request 파라미터 다 읽어와 map에 저장)
+        Map<String, String> paramMap = createParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        String viewName = mv.getViewName();// 논리 이름 ex. new-form
+        MyView view = viewResolver(viewName);
+
+        view.render(mv.getModel(), request, response);
+
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
