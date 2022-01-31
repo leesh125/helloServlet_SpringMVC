31951f7d71a5182e641a19f8d332a0fc7471dfe5
leesh125 <chelsea12345@naver.com>
2022-01-31 18:58:46 +0900

    단순하고 실용적인 컨트롤러(v4) - 프론트 컨트롤러에서 해당 서비스 컨트롤러를 호출할 때 model객체를 같이 넘겨준다. -> 해당 컨트롤러에서 model 객체에 값을 넣어주고 viewName을 반환 -> viewName을 통해 model을 같이 넘겨주면서 view를 랜더링한다.

---
+++ src/main/java/hello/servlet/web/frontcontroller/v4/FrontControllerServletV4.java
@@ -0,0 +1,67 @@
+package hello.servlet.web.frontcontroller.v4;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// v1 하위에 어떤 url이 들어와도 일단 이 서블릿이 호출됨
+@WebServlet(name = "frontControllerServletV4", urlPatterns = "/front-controller/v4/*")
+public class FrontControllerServletV4 extends HttpServlet {
+
+    private Map<String, ControllerV4> controllerV4Map = new HashMap<>();
+
+    public FrontControllerServletV4() {
+        // 매개변수 1 url 요청이 오면 , MemberFormControllerV1 생성
+        controllerV4Map.put("/front-controller/v4/members/new-form", new MemberFormControllerV4());
+        controllerV4Map.put("/front-controller/v4/members/save", new MemberSaveControllerV4());
+        controllerV4Map.put("/front-controller/v4/members", new MemberListControllerV4());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI();// 들어온 URL을 얻을 수 있다.
+
+        ControllerV4 controller = controllerV4Map.get(requestURI); // 요청 온 URI에 의해 Controller 객체가 다르게 호출
+        if(controller == null){ // 읽어온 URI에 매치되는 Controller 객체가 없다면
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND); // 404 Not Found
+            return;
+        }
+
+        // paramMap 넘겨주기 ( request 파라미터 다 읽어와 map에 저장)
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>(); // 추가
+        String viewName = controller.process(paramMap, model);
+
+        MyView view = viewResolver(viewName);
+
+        view.render(model, request, response);
+
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
