5c130cee06b39a8d05ae40418bd94b0f7c011fa7
leesh125 <chelsea12345@naver.com>
2022-02-01 18:03:26 +0900

    스프링 MVC(시작하기) - @RequestMapping 애노테이션을 사용하는 컨트롤러를 통해 요청 정보를 매핑하고 해당 url이 호출되면@RequestMapping을 적용한 ModelAndView 메서드가 실행된다. ModelAndView메서드에선 모델과 뷰 정보를 담아서 반환

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 // 프로젝트 패키지의 하위 패키지들 중에 서블릿을 찾아서 자동으로 등록한다
 @ServletComponentScan // 서블릿 자동 등록
@@ -12,5 +14,10 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
+/*	스프링 빈 수동 설정으로 @Controller 없이 @RequestMapping 만으로 핸들러 매핑
+	@Bean
+	SpringMemberFormControllerV1 springMemberFormControllerV1(){
+		return new SpringMemberFormControllerV1();
+	}
+*/
 }
